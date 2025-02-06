24e6b573b576e967ccd1ba4db21ae5fb59463bee
Gromov Andrew <gromov7397@yandex.ru>
2025-02-06 20:24:07 +0300

    переписал app на class

--- todo-mvc/src/components/app/app.js
+++ todo-mvc/src/components/app/app.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import './app.css';
 import AppHeader from '../app-header';
 import TaskList from '../task-list';
 import Footer from '../footer';
 
-const App = () => {
-    const todoData = [
-        {
-            //status: 'completed',
-            description: 'Completed task',
-            created: 'created 17 seconds ago'
-        },
-        {
-            //status: 'editing',
-            description: 'Editing task',
-            created: 'created 5 minutes ago'
-        },
-        {
-            //status: 'active',
-            description: 'Active task',
-            created: 'created 5 minutes ago'
-        }
-    ];
+export default class App extends Component {
+    state = {
+        todoData : [
+            {
+                //status: 'completed',
+                description: 'Completed task',
+                created: 'created 17 seconds ago'
+            },
+            {
+                //status: 'editing',
+                description: 'Editing task',
+                created: 'created 5 minutes ago'
+            },
+            {
+                //status: 'active',
+                description: 'Active task',
+                created: 'created 5 minutes ago'
+            }
+        ]
+    }
 
-    return (
-        <>
-            <AppHeader />
-            <section className="main">
-                <TaskList todos={todoData} />
-                <Footer />
-            </section>
-        </>
-    );
+    render() {
+        return (
+            <>
+                <AppHeader />
+                <section className="main">
+                    <TaskList todos={this.state.todoData} />
+                    <Footer />
+                </section>
+            </>
+        );
+    }
 };
-
-export default App;
