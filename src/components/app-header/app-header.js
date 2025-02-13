9ab24086b6362b3ca86bf24856fc20baaaf6c76b
Gromov Andrew <gromov7397@yandex.ru>
2025-02-13 23:18:58 +0300

    test

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './app-header.css';
+
+export default class AppHeader extends Component {
+  render() {
+    const { onTaskAdded } = this.props;
+
+    return (
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' && e.target.value) {
+              onTaskAdded(e.target.value);
+              e.target.value = ''; // Очистка поля
+            }
+          }}
+        />
+      </header>
+    );
+  }
+}
+
+AppHeader.defaultProps = {
+  onTaskAdded: () => {},
+};
+
+AppHeader.propTypes = {
+  onTaskAdded: PropTypes.func,
+};
